87a477479efb26faff28a7b721aec9280ab66b0f
AlisaBrouka <brovko.alisa@gmail.com>
2024-12-21 12:42:07 +0300

    Final version of rock-paper-scissors

--- rps_for_git.py
+++ rps_for_git.py
@@ -2,47 +2,107 @@ import sys
 import random
 from enum import Enum
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCRISSORS = 3
+def rps(name = "PlayerOne"):
 
-playagain = True
+    game_count = 0
+    player_wins = 0
+    python_wins = 0
 
-while playagain:
+    def play_rps():
 
-    playerchoice = input("""
-    Enter:
-    1 for rock
-    2 for paper
-    3 for scrissors
-    """)
+        nonlocal name
+        nonlocal game_count 
+        nonlocal player_wins
+        nonlocal python_wins
 
-    player = int(playerchoice)
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCRISSORS = 3
 
-    if player < 1 or player > 3:
-        sys.exit("You must enter 1, 2 or 3.")
+        playerchoice = input(f"""{name}, enter:
+        1 for rock
+        2 for paper
+        3 for scrissors
+        """)
 
-    computerchoise = random.choice("123")
-    computer = int(computerchoise)
+        if playerchoice not in "123":
+            print(f"{name}, please enter 1, 2 or 3.")
+            return play_rps()
 
-    print("You chose: " + str(RPS(player)).replace("RPS.", ""))
-    print("Computer chose: " + str(RPS(computer)).replace("RPS.", ""))
+        player = int(playerchoice)
 
-    if player == computer:
-        print("It's a tie!")
-    elif player == 1 and computer == 3:
-        print("You win!")
-    elif player == 2 and computer == 1:
-        print("You win!")
-    elif player == 3 and computer == 2:
-        print("You win!")
-    else: print("Python wins!")
+        if player < 1 or player > 3:
+            sys.exit("You must enter 1, 2 or 3.")
 
-    playagain = input('\nPlay again? \nY for Yes\nQ for Quit\n')
-    if playagain.lower() == "y":
-        continue
-    else: 
-        print("\nThank you for playing!")
-        playagain = False
-sys.exit("Bye!")
+        computerchoise = random.choice("123")
+        computer = int(computerchoise)
+
+        print(
+            f"{name}, you chose: {str(RPS(player)).replace("RPS.", "").title()}"
+            )
+        print(
+            f"Computer chose: {str(RPS(computer)).replace("RPS.", "").title()}"
+            )
+
+        def decide_winner(player, computer):
+
+            nonlocal name
+            nonlocal player_wins
+            nonlocal python_wins
+
+            if player == computer:
+                return "It's a tie!"
+            elif player == 1 and computer == 3:
+                player_wins += 1
+                return f"{name}, you win!"
+            elif player == 2 and computer == 1:
+                player_wins += 1
+                return f"{name}, you win!"
+            elif player == 3 and computer == 2:
+                player_wins += 1
+                return f"{name}, you win!"
+            else: 
+                python_wins += 1
+                return f"Python wins! \n Sorry, {name}"
+
+        game_result = decide_winner(player, computer)
+        print(game_result)
+        
+        game_count += 1
+
+        print(f"\n Game count: {game_count}")
+        print(f"\n {name}'s wins: {player_wins}")
+        print(f"\n Python wins: {python_wins}")
+
+        print(f"\nPlay again, {name}?")
+        while True:
+            playagain = input('\nY for Yes\nQ for Quit\n')
+            if playagain .lower() not in ['y', 'q']:
+                continue
+            else: 
+                break
+        if playagain.lower() == "y":
+            return play_rps()
+        else: 
+            print("\nThank you for playing!")
+            sys.exit(f"Bye, {name}!")
+
+    return play_rps
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Provides a personalized experience"
+    )
+
+    parser.add_argument(
+        "-n", "--name", metavar="name",
+        required=True, help="The name of the person playing the game."
+    )
+
+    args = parser.parse_args()
+
+    rock_paper_scissors = rps(args.name)
+    rock_paper_scissors()
